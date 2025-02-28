3e363d87471dd3981b51d2b7625f2fad52c52aee
SZL741023 <shengzhe.lin@icloud.com>
2025-02-28 16:11:20 +0800

    [Env setup] add package

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "bootstrap/dist/css/bootstrap.css";
+import { HashRouter } from "react-router-dom";
 import "./styles/_styles.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
