2f3336a08fd8d42f72dcf18222223a3d0719a127
SZL741023 <shengzhe.lin@icloud.com>
2025-03-12 23:57:01 +0800

    [Page] Done the banner of home page.
    Fixed the x scroll bar of home page.
    Done the banner section of home page.

--- src/components/DownDecorate.jsx
+++ src/components/DownDecorate.jsx
@@ -1,7 +1,7 @@
 import DownImage from "/images/down.svg";
 const DownDecorate = () => {
   return (
-    <div className="d-flex position-absolute bottom-0 start-0">
+    <div className="d-flex position-absolute bottom-0 start-0 overflow-hidden">
       <img src={DownImage} alt="" />
       <img src={DownImage} alt="" />
       <img src={DownImage} alt="" />
